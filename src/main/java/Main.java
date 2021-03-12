68b4ce15eb32d3e6385446d605f5c1cd332c08d3
travellerwoman <63811457+mpr-7@users.noreply.github.com>
2021-03-12 11:56:45 +0100

    basic functions

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void Main(String[] args) {
+        Calculator calculator = new Calculator();
+        int result;
+        result = calculator.add(2,3);
+        System.out.println("Add: "+result);
+        result = calculator.substract(4, 5);
+        System.out.println("Substract: "+result);
+
+    }
+
+}
