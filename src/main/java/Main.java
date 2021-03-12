49eb3bcaac6ae6ac390ff56b35eb4e7d5302250e
DrakeMistBorn <40219320+DrakeMistBorn@users.noreply.github.com>
2021-03-12 12:12:50 +0100

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,11 @@
 
 public class Main {
-    public static void Main(String[] args) {
+    public static void main(String[] args) {
         Calculator calculator = new Calculator();
         int result;
         result = calculator.add(2,3);
         System.out.println("Add: "+result);
         result = calculator.substract(4, 5);
         System.out.println("Substract: "+result);
-
     }
-
 }
