240628a09029baef304eb6675ad7464394bf10bb
ysayansrom <74519312+yacarsan@users.noreply.github.com>
2021-03-13 21:13:29 +0100

    Add and substraction of 2 matrices, substract 2 matrices with negative values as result , add and substract with different dimensions, add and substract without filling one matrix

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -140,6 +140,98 @@ class CalculatorTest {
         Assertions.assertEquals(expectedRes,res);
     }
 
-    //todo: add/substract 2 matrices with different dimensions
+    @Test
+    void add2MatricesWithDiffernentDimensions(){
+        int row = 4;
+        int column = 4;
+        int row2 = 3;
+        int column2 = 3;
+        int valueMat1 = 2;
+        int valueMat2 = 1;
+        Calculator calc = new Calculator();
+        MatrixOperation matrix1 = new MatrixOperation(row,column);
+        MatrixOperation matrix2 = new MatrixOperation(row2,column2);
 
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                matrix1.setValue(i, j, valueMat1);
+            }
+        }
+        for (int i = 0; i < row2; i++) {
+            for (int j = 0; j < column2; j++) {
+                matrix2.setValue(i, j, valueMat2);
+            }
+        }
+        Assertions.assertNull(calc.add(matrix1,matrix2));
+    }
+
+
+    @Test
+    void substract2MatricesWithDiffernentDimensions(){
+        int row = 4;
+        int column = 4;
+        int row2 = 3;
+        int column2 = 3;
+        int valueMat1 = 2;
+        int valueMat2 = 1;
+        Calculator calc = new Calculator();
+        MatrixOperation matrix1 = new MatrixOperation(row,column);
+        MatrixOperation matrix2 = new MatrixOperation(row2,column2);
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                matrix1.setValue(i, j, valueMat1);
+            }
+        }
+        for (int i = 0; i < row2; i++) {
+            for (int j = 0; j < column2; j++) {
+                matrix2.setValue(i, j, valueMat2);
+            }
+        }
+        Assertions.assertNull(calc.substract(matrix1,matrix2));
+    }
+
+    @Test
+    void add2MatricesWithoutFillingTheSecondOne(){
+        int row = 4;
+        int column = 4;
+        int valueMat = 2;
+        Calculator calc = new Calculator();
+        String res,expectedRes;
+        MatrixOperation matrix1 = new MatrixOperation(row,column);
+        MatrixOperation matrix2 = new MatrixOperation(row,column);
+        MatrixOperation matrix3 = new MatrixOperation(row,column);
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                matrix1.setValue(i, j, valueMat);
+                matrix3.setValue(i, j, valueMat);
+            }
+        }
+        res = calc.add(matrix1,matrix2).toString();
+        expectedRes = matrix3.toString();
+        Assertions.assertEquals(expectedRes,res);
+    }
+
+    @Test
+    void substract2MatricesWithoutFillingTheSecondOne(){
+        int row = 4;
+        int column = 4;
+        int valueMat = 2;
+        Calculator calc = new Calculator();
+        String res,expectedRes;
+        MatrixOperation matrix1 = new MatrixOperation(row,column);
+        MatrixOperation matrix2 = new MatrixOperation(row,column);
+        MatrixOperation matrix3 = new MatrixOperation(row,column);
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                matrix1.setValue(i, j, valueMat);
+                matrix3.setValue(i, j, valueMat);
+            }
+        }
+        res = calc.substract(matrix1,matrix2).toString();
+        expectedRes = matrix3.toString();
+        Assertions.assertEquals(expectedRes,res);
+    }
 }
