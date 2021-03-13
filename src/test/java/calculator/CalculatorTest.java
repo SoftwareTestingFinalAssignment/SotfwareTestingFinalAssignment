e4e51e122a0c55cbc49b359463bf22b478a2e6f8
ysayansrom <74519312+yacarsan@users.noreply.github.com>
2021-03-13 19:24:33 +0100

    Test of substraction with negative values as result and corrections of hard-coded values

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -10,31 +10,51 @@ class CalculatorTest {
     @Test
     void add2Numbers() {
         Calculator calc = new Calculator();
-        Assertions.assertEquals(3, calc.add(1,2));
+        double expectedRes = 3;
+        double value1 = 1;
+        double value2 = 2;
+        double res = calc.add(value1,value2);
+        Assertions.assertEquals(expectedRes, res);
     }
 
     @Test
     void substract2Numbers(){
         Calculator calc = new Calculator();
-        Assertions.assertEquals(2, calc.substract(3,1));
+        double expectedRes = 2;
+        double value1 = 3;
+        double value2 = 1;
+        double res = calc.substract(value1,value2);
+        Assertions.assertEquals(expectedRes, res);
     }
 
     @Test
     void multiply2Numbers(){
         Calculator calc = new Calculator();
-        Assertions.assertEquals(6, calc.multiply(3,2));
+        double expectedRes = 6;
+        double value1 = 3;
+        double value2 = 2;
+        double res = calc.multiply(value1,value2);
+        Assertions.assertEquals(expectedRes, res);
     }
 
     @Test
     void multiplyBy0(){
         Calculator calc = new Calculator();
-        Assertions.assertEquals(0, calc.multiply(3,0));
+        double expectedRes = 0;
+        double value1 = 3;
+        double value2 = 0;
+        double res = calc.multiply(value1,value2);
+        Assertions.assertEquals(expectedRes, res);
     }
 
     @Test
     void divide2Numbers(){
         Calculator calc = new Calculator();
-        Assertions.assertEquals(2, calc.divide(6,3));
+        double expectedRes = 2;
+        double value1 = 6;
+        double value2 = 3;
+        double res = calc.divide(value1,value2);
+        Assertions.assertEquals(expectedRes, res);
     }
 
     /*
@@ -54,16 +74,20 @@ class CalculatorTest {
     void add2Matrices(){
         int row = 4;
         int column = 4;
+        int valueMat1 = 2;
+        int valueMat2 = 1;
+        int valueMat3 = 3;
         Calculator calc = new Calculator();
         String res,expectedRes;
         MatrixOperation matrix1 = new MatrixOperation(row,column);
         MatrixOperation matrix2 = new MatrixOperation(row,column);
         MatrixOperation matrix3 = new MatrixOperation(row,column);
+
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < column; j++) {
-                matrix1.setValue(i, j, 2);
-                matrix2.setValue(i, j, 1);
-                matrix3.setValue(i, j, 3);
+                matrix1.setValue(i, j, valueMat1);
+                matrix2.setValue(i, j, valueMat2);
+                matrix3.setValue(i, j, valueMat3);
             }
         }
         res = calc.add(matrix1,matrix2).toString();
@@ -75,16 +99,45 @@ class CalculatorTest {
     void substract2Matrices(){
         int row = 4;
         int column = 4;
+        int valueMat1 = 2;
+        int valueMat2 = 1;
+        int valueMat3 = 3;
         Calculator calc = new Calculator();
         String res,expectedRes;
         MatrixOperation matrix1 = new MatrixOperation(row,column);
         MatrixOperation matrix2 = new MatrixOperation(row,column);
         MatrixOperation matrix3 = new MatrixOperation(row,column);
+
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < column; j++) {
-                matrix1.setValue(i, j, 3);
-                matrix2.setValue(i, j, 1);
-                matrix3.setValue(i, j, 2);
+                matrix1.setValue(i, j, valueMat1);
+                matrix2.setValue(i, j, valueMat2);
+                matrix3.setValue(i, j, valueMat3);
+            }
+        }
+        res = calc.substract(matrix1,matrix2).toString();
+        expectedRes = matrix3.toString();
+        Assertions.assertEquals(expectedRes,res);
+    }
+
+    @Test
+    void substract2MatricesWithNegativeValuesAsResult(){
+        int row = 4;
+        int column = 4;
+        int valueMat1 = 2;
+        int valueMat2 = 3;
+        int valueMat3 = -1;
+        Calculator calc = new Calculator();
+        String res,expectedRes;
+        MatrixOperation matrix1 = new MatrixOperation(row,column);
+        MatrixOperation matrix2 = new MatrixOperation(row,column);
+        MatrixOperation matrix3 = new MatrixOperation(row,column);
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                matrix1.setValue(i, j, valueMat1);
+                matrix2.setValue(i, j, valueMat2);
+                matrix3.setValue(i, j, valueMat3);
             }
         }
         res = calc.substract(matrix1,matrix2).toString();
