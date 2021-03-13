caa5ec0a60f60dc9aed326889ba57227e9215bb1
ysayansrom <74519312+yacarsan@users.noreply.github.com>
2021-03-13 19:09:11 +0100

    Test of addition and substraction of matrices

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -2,41 +2,43 @@ package calculator;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 
 class CalculatorTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void add2Numbers() {
         Calculator calc = new Calculator();
         Assertions.assertEquals(3, calc.add(1,2));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void substract2Numbers(){
         Calculator calc = new Calculator();
         Assertions.assertEquals(2, calc.substract(3,1));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void multiply2Numbers(){
         Calculator calc = new Calculator();
         Assertions.assertEquals(6, calc.multiply(3,2));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void multiplyBy0(){
         Calculator calc = new Calculator();
         Assertions.assertEquals(0, calc.multiply(3,0));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void divide2Numbers(){
         Calculator calc = new Calculator();
         Assertions.assertEquals(2, calc.divide(6,3));
     }
 
-    /*@org.junit.jupiter.api.Test
+    /*
+    @Test
     void divideBy0(){
         //This checks for the arithmetic exception when we divide by 0
         //We wrote Exception.class so we were able to capture more exceptions
@@ -45,6 +47,51 @@ class CalculatorTest {
         Assert.assertThrows(Exception.class, () -> {
             calc.divide(1,0);
         });
-    }*/
+    }
+    */
+
+    @Test
+    void add2Matrices(){
+        int row = 4;
+        int column = 4;
+        Calculator calc = new Calculator();
+        String res,expectedRes;
+        MatrixOperation matrix1 = new MatrixOperation(row,column);
+        MatrixOperation matrix2 = new MatrixOperation(row,column);
+        MatrixOperation matrix3 = new MatrixOperation(row,column);
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                matrix1.setValue(i, j, 2);
+                matrix2.setValue(i, j, 1);
+                matrix3.setValue(i, j, 3);
+            }
+        }
+        res = calc.add(matrix1,matrix2).toString();
+        expectedRes = matrix3.toString();
+        Assertions.assertEquals(expectedRes,res);
+    }
+
+    @Test
+    void substract2Matrices(){
+        int row = 4;
+        int column = 4;
+        Calculator calc = new Calculator();
+        String res,expectedRes;
+        MatrixOperation matrix1 = new MatrixOperation(row,column);
+        MatrixOperation matrix2 = new MatrixOperation(row,column);
+        MatrixOperation matrix3 = new MatrixOperation(row,column);
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                matrix1.setValue(i, j, 3);
+                matrix2.setValue(i, j, 1);
+                matrix3.setValue(i, j, 2);
+            }
+        }
+        res = calc.substract(matrix1,matrix2).toString();
+        expectedRes = matrix3.toString();
+        Assertions.assertEquals(expectedRes,res);
+    }
+
+    //todo: add/substract 2 matrices with different dimensions
 
 }
